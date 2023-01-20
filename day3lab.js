5ce5b1ea2295a9fe74812093c94dffb5ac65f199
Vaishali Trivedi <vaishalitrivedi21@gmail.com>
2023-01-20 12:08:16 -0500

    first commit

---
+++ day3lab.js
@@ -0,0 +1,37 @@
+//What is a Single Page Application?
+
+// Ans. A single-page application is an application that loads a single HTML page and 
+//all the necessary assets (such as JavaScript and CSS) required for the application to run.
+
+
+//What is REACT?
+
+//Ans. React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library
+// for building user interfaces based on UI components. It is maintained by Meta (formerly Facebook) 
+//and a community of individual developers and companies.React can be used as a base in the 
+//development of single-page, mobile, or server-rendered applications.
+//However, React is only concerned with state management and rendering that state to the DOM.
+
+//What is JSX?
+
+//Ans. JSX stands for JavaScript XML.
+// JSX allows us to write HTML in React.
+//JSX makes it easier to write and add HTML in React.
+
+//What is Babel?
+
+//Ans Babel is a JavaScript compiler.
+// it can convert the latest version of JavaScript code into the one that the browser understands.
+
+//What is Webpack?
+
+//Ans. React Webpack is also a command-line tool used to create a bundle of assets (files and code). 
+//It doesn't run on the browser or the server. 
+//It takes all the JS files and other assets, transforming them into one large file.
+
+//What is Create React App?
+
+//Ans. Create React App is a comfortable environment for learning React, and 
+//is the best way to start building a new single-page application in React. 
+//It sets up your development environment so that you can use the latest JavaScript features, 
+//provides a nice developer experience, and optimizes your app for production.
